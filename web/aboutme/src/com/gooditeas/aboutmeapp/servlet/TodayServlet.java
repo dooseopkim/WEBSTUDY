6c47212cca1ad0a982d02e1b3059773d47f61cb7
jihwanma <qwq713@naver.com>
2019-02-16 10:54:57 +0900

    [2주차-aboutme]

--- web/aboutme/src/com/gooditeas/aboutmeapp/servlet/TodayServlet.java
+++ web/aboutme/src/com/gooditeas/aboutmeapp/servlet/TodayServlet.java
@@ -1,6 +1,10 @@
 package com.gooditeas.aboutmeapp.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,24 +14,47 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class TodayServlet
  */
-@WebServlet(description = "show time at now", urlPatterns = { "/today" })
+@WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
+      
     public TodayServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+    	super();
     }
-
+    
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	    
+	    //request.setCharacterEncoding("UTF-8");
+	    response.setContentType("text/html;charset=UTF-8");
+	    
+	    
+	    LocalDateTime nowDateTime = LocalDateTime.now();
+	    
+	    PrintWriter printWriter = response.getWriter();
+	    printWriter.print(buildHtmlSource(nowDateTime));
 	}
-
+    
+	
+	private static String buildHtmlSource(LocalDateTime nowDateTime) {
+	    StringBuilder sb = new StringBuilder();
+	    String formattedDateTime = nowDateTime.format(dateTimeFormatter);
+	    String frontHtml ="<!DOCTYPE html>\n" + 
+	            "<html lang=\"ko\">\n" + 
+	            "<head>\n" + 
+	            "    <meta charset=\"UTF-8\">\n" + 
+	            "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" + 
+	            "    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n" + 
+	            "    <title>today</title>\n" + 
+	            "</head>\n" + 
+	            "<body>\n" + 
+	            "    <a href = \"/aboutme\"> 메인으로 </a><br/>\n" + 
+	            "";
+	    String backHtml = "</body>\n" + "</html>";
+	    return sb.append(frontHtml).append(formattedDateTime).append(backHtml).toString();
+	}
 }
