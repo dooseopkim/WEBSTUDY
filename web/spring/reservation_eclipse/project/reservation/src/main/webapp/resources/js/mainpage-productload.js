02f00e81bb079c80845ba7391fbe7fd934627763
jihwan <qwq713@naver.com>
2019-04-15 21:52:35 +0900

    [2019/04/15 MON] 3. 상위 4개만 보여주고 나머지 숨기기 4. 더보기 버튼 만들기 5. 더보기 버튼 클릭 시 4개씩 더 보여주기 6. 더이상 보여줄 데이터가 없을 시 더보기 버튼 없애기

--- web/spring/reservation_eclipse/project/reservation/src/main/webapp/resources/js/mainpage-productload.js
+++ web/spring/reservation_eclipse/project/reservation/src/main/webapp/resources/js/mainpage-productload.js
@@ -1,8 +1,27 @@
-var productList;
+
 const productBoxes = document.querySelectorAll(".lst_event_box");
 const leftProductListBox = productBoxes[0];
 const rightProductListBox = productBoxes[1];
 
+const PRODUCTS_PER_CLICK = 4;
+
+var productMaxCount = -1;
+var exposedProductCount = 0;
+var productItemsInfo;
+
+
+
+const makeProductItemHTML = function(elt){
+    let itemTemplate = document.querySelector("#itemList").innerHTML;
+    itemTemplate =  itemTemplate.replace('{{id}}',elt.productId)
+                                .replace('{{id}}',elt.productId)
+                                .replace('{{description}}',elt.productDescription)
+                                .replace('{{description}}',elt.productDescription)
+                                .replace('{{productImageUrl}}',elt.productImageUrl)
+                                .replace('{{content}}',elt.productContent)
+                                .replace('{{placeName}}',elt.placeName);
+    return itemTemplate;
+};
 
 const fetchProductList = function(categoryId, start){
     let requestURL = "api/products";
@@ -19,30 +38,44 @@ const fetchProductList = function(categoryId, start){
 
         let idx = 0;
         if(200<=reqStatus && reqStatus < 300){
-            productList = JSON.parse(httpRequest.responseText);
-            productList.items.forEach((elt)=>{
-                let itemTplHTML = document.querySelector("#itemList").innerHTML;
-                // id , description , content ,placeName
-                itemTplHTML = itemTplHTML.replace('{{id}}',elt.productId)
-                .replace('{{id}}',elt.productId)
-                .replace('{{description}}',elt.productDescription)
-                .replace('{{description}}',elt.productDescription)
-                .replace('{{productImageUrl}}',elt.productImageUrl)
-                .replace('{{content}}',elt.productContent)
-                .replace('{{placeName}}',elt.placeName);
+            let productInfo = JSON.parse(httpRequest.responseText);
+            productMaxCount = productInfo.totalCount;
+            productItemsInfo = productInfo.items;
+
+            for(let idx = 0; idx < PRODUCTS_PER_CLICK; idx++){
+                let itemTplHTML = makeProductItemHTML(productItemsInfo[idx]);
                 if(idx%2 == 0){
                     leftProductListBox.innerHTML += itemTplHTML;
                 }else{
                     rightProductListBox.innerHTML += itemTplHTML;
                 }
-                console.log(idx + " " +itemTplHTML);
-                idx++;
-            });
+            }
+            exposedProductCount += PRODUCTS_PER_CLICK;
         }
     };
     httpRequest.open(httpMethod,requestURL);
     httpRequest.send();
 };
 
+const moreViewEvent = function(){
+    // exposedProductCount ~ exposedProductCount+4 까지 추가로 노출.
+    // 만약 productMaxCount와 인덱스가 같아질 경우 더보기 버튼 삭제.
+    let itemIdx;
+    for(itemIdx = exposedProductCount; itemIdx < exposedProductCount+4 && itemIdx < productMaxCount; itemIdx++){
+        let itemTplHTML = makeProductItemHTML(productItemsInfo[itemIdx]);
+        if(itemIdx%2 == 0){
+            leftProductListBox.innerHTML += itemTplHTML;
+        }else{
+            rightProductListBox.innerHTML += itemTplHTML;
+        }
+    }
+    if(itemIdx == productMaxCount){
+        //remove 더보기 버튼
+        document.querySelector(".more").remove();
+    }
+    exposedProductCount += PRODUCTS_PER_CLICK;
+}
 
+fetchProductList("",0);
+document.querySelector(".more").addEventListener('click',moreViewEvent);
 
